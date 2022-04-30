86ab79a45860fc44c5d2f314cb6c73b376245812
9z <slts0405@gmail.com>
2022-04-30 23:24:15 +0900

    초기 데이터 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Jackson Blog</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">글목록</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <h5>Vue 개발자 블로그</h5>
+    <p>vue 공부중</p>
+  </div>
+
+  <PostList :글="글" v-for="(글, i) in 블로그글" :key="i"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostList from "./components/PostList.vue";
+import blog from './assets/blog-posts.js'
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      블로그글: blog,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    PostList,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +54,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h5>{{ 글.title }}</h5>
+      <p>{{ 글.content }}</p>
+      <p>{{ 글.date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PostList',
+    props: {
+        글: Object
+    }
+}
+</script>
+
+<style>
+
+</style>
